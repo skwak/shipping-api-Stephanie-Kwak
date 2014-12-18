c0c3a147f7bcb3bae1ca9a4b60585a0942f0eaab
Stephanie Kwak <stephanieyoungkwak@gmail.com>
2014-12-18 12:02:52 -0800

    parse the ups data better

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -3,17 +3,34 @@ class QuotesController < ApplicationController
   include ActiveMerchant::Shipping
 
   def search
+    
+    @package = Package.new(12,
+                          [93,10],
+                          :cylinder => false
+                          )
 
-    @package = Package.new(100, [93,10], :cylinder => true)
-    @origin = Location.new(:country => "US",
-                           :state => "WA",
-                           :city => "Seattle",
-                           :zip => "98121")
+    @origin = Location.new(:country => params["origincountry"],
+                           :state => params["originstate"],
+                           :city => params["origincity"],
+                           :zip => params["originzip"]
+                           )
+
     @destination = Location.new(:country => params["country"],
                                 :state => params["state"],
                                 :city => params["city"],
-                                :zip => params["zip"])
-    # http://localhost:3000/quotes/search?country=US&state=MD&city=Baltimore&zip=21231
+                                :zip => params["zip"]
+                                )
+
+    @carrier = params["carrier"]
+
+    if @carrier == "UPS"
+      ups = UPS.new(:login => ENV["UPS_LOGIN"], :password => ENV["UPS_PASSWORD"], :key => ENV["UPS_ACCESS_KEY"])
+      response = ups.find_rates(@origin, @destination, @package)
+      ups_rates = response.rates.sort_by(&:price).collect {|rate| [rate.service_name, rate.price]}
+      render json: ups_rates
+    end
+
+    # http://localhost:3000/quotes/search?origincountry=US&originstate=WA&origincity=Seattle&originzip=98121&country=US&state=MD&city=Baltimore&zip=21231&carrier=UPS
 
     # @package = Package.new(  100,                        # 100 grams
     #   [93,10],                    # 93 cm long, 10 cm diameter
@@ -29,9 +46,6 @@ class QuotesController < ApplicationController
     # :city => 'Ottawa',
     # :postal_code => 'K1P 1J1')
     #
-    ups = UPS.new(:login => ENV["UPS_LOGIN"], :password => ENV["UPS_PASSWORD"], :key => ENV["UPS_ACCESS_KEY"])
-    response = ups.find_rates(@origin, @destination, @package)
-    render json: response
 
     # usps = USPS.new(:login => ENV["USPS_USERNAME"])
     # response = usps.find_rates(@origin, @destination, @package)
